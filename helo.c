fe6bef68556c7f103a7249bdb69c770fcc162231
acousel <deepak17122002@gmail.com>
2023-07-21 21:54:36 +0530

    first

---
+++ helo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  FILE *txt;
+  txt = fopen("lol.txt", "w");
+  //this is a 
+
+
+
+  char str[50];
+
+  fputs("t fjefoiafj eis is testing deepak bharti", txt);
+
+  // while (fgets(str,40, txt)!=NULL){
+
+  //  printf("%s\n", str);
+
+  return 0;
+}
+
+
+swwpK ShvhwuiedwdHWDLHOIDW ddl;DJKWDIOPDJ;WOPODSDl;djopwdwudpwoiueiouu  iop IPDUPDU ;djiwoD
+D;WJDWOPDIWDUDL;JFPOAIEIOPFUEF;LJEFPIOAEFAKL;JDFVANSF;EFAEPIF
+AFJEPFOIEFUIEPFUIEPOF
+FJFPEWFIEFEUWPOFF;LJFPWEOIFUAF
+AEFEFJEPOFAIPFEDFUIPUAWPOIELKFJWEPAI
+DEEPAK BAHRTI IS THE CLASS REP AND 
